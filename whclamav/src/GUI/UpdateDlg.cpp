22c618426d606da2d18cdbf44dfc145f77c154b8
darkhawk665 <darkhawk665@91a5605e-6c32-0410-b856-ac5e193a5ff4>
2008-11-24 12:17:31 +0000

    ADD: UpdateDialog Thread, Clean button.

--- whclamav/src/GUI/UpdateDlg.cpp
+++ whclamav/src/GUI/UpdateDlg.cpp
@@ -8,28 +8,32 @@ UpdateDlg::UpdateDlg(wxWindow *parent) : wxDialog(parent,wxID_ANY,wxT("Update"))
 {
 
   wxBoxSizer *box = new wxBoxSizer(wxVERTICAL);
+  wxBoxSizer *ctr = new wxBoxSizer(wxHORIZONTAL);
 
     m_msg = new wxTextCtrl(this, wxID_ANY,wxT(""), wxDefaultPosition,wxDefaultSize,wxTE_MULTILINE);
 
     m_msg->SetEditable(false);
 
+    ctr->Add( new wxButton(this,wxID_UP,wxT("&Update")),0,wxALIGN_CENTER_HORIZONTAL|wxALL,5);
+    ctr->Add( new wxButton(this,wxID_CLEAR,wxT("&Clear")),0,wxALIGN_CENTER_HORIZONTAL|wxALL,5);
+
     box->Add(m_msg,1,wxEXPAND|wxALL,5);
-    box->Add( new wxButton(this,wxID_OK,wxT("&Update")),0,wxALIGN_CENTER_HORIZONTAL|wxALL,5);
+    box->Add(ctr,0,wxALIGN_CENTER_HORIZONTAL|wxALL,5);
+
+
 
     this->SetSizer(box);
     this->Layout();
 }
 
 
-void UpdateDlg::onStartUpdate(wxCommandEvent &evt)
+void UpdateDlg::run()
 {
+FILE *fl;
+char c;
+wxString str;
 
-  FILE *fl;
-  wxString str;
-  char c;
-
-        m_msg->SetValue(wxT(""));
-        fl = popen("freshclam","r");
+        fl = popen("freshclam 2>&1","r");
         this->SetTitle( wxT("Updating please wait"));
 
         do
@@ -42,9 +46,35 @@ void UpdateDlg::onStartUpdate(wxCommandEvent &evt)
 
         pclose(fl);
         m_msg->SetValue( m_msg->GetValue() + str);
-        this->SetTitle( wxT("Update"));
+
+
+}
+
+
+void UpdateDlg::onStartUpdate(wxCommandEvent &evt)
+{
+
+        m_msg->Clear();
+        this->SetTitle( wxT("Updating please wait"));
+        this->startThread();
+
+
+}
+
+
+void UpdateDlg::onClear(wxCommandEvent &evt)
+{
+    m_msg->Clear();
+}
+
+void UpdateDlg::onTerminate()
+{
+    wxMutexGuiEnter();
+    this->SetTitle( wxT("Updated"));
+    wxMutexGuiLeave();
 }
 
 BEGIN_EVENT_TABLE(UpdateDlg,wxDialog)
-EVT_BUTTON(wxID_OK,UpdateDlg::onStartUpdate)
+EVT_BUTTON(wxID_UP,UpdateDlg::onStartUpdate)
+EVT_BUTTON(wxID_CLEAR,UpdateDlg::onClear)
 END_EVENT_TABLE()
