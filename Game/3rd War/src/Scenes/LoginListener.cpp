c6615e854925e1c2b90825307d98adfcf4789857
darkhawk665 <darkhawk665@91a5605e-6c32-0410-b856-ac5e193a5ff4>
2010-06-14 22:21:25 +0000

    - Added friction

--- Game/3rd War/src/Scenes/LoginListener.cpp
+++ Game/3rd War/src/Scenes/LoginListener.cpp
@@ -8,43 +8,31 @@ LoginListener::LoginListener(RenderWindow* win, Camera* cam, Real time_step, Ogr
             m_world   = world;
             m_cam     = cam;
 
-
             m_stepper = new OgreOde::StepHandler(m_world, OgreOde::StepHandler::QuickStep, time_step, 0.25, 1);
             m_stepper = new OgreOde::ForwardFixedStepHandler(m_world, StepHandler::QuickStep, time_step, 0.25, 1);
             m_stepper->setAutomatic(OgreOde::StepHandler::AutoMode_PostFrame, Ogre::Root::getSingletonPtr());
             Root::getSingleton().setFrameSmoothingPeriod(5.0f);
 
-
-
             dummy = new GameStaticPhysicObject(world,"Dummy.mesh","",0.001,Vector3(2,2,2));
             dummy->setPosition(Vector3(102,30,-62));
-
 }
-
 
 
 
 bool LoginListener::frameStarted(const FrameEvent& evt)
 {
-
         mKeyboard->capture();
         mMouse->capture();
 
         if (mTimeUntilNextToggle <= 0)
             mTimeUntilNextToggle = 0.5;
 
-        /*if(!m_stepper->isPaused())
-              for( std::list<Truck*>::iterator it = m_trucks.begin(); it != m_trucks.end(); it++)
-                          (*it)->frameStarted(evt);*/
-
         return true;
 }
 
 
-
 bool LoginListener::processUnbufferedKeyInput(const FrameEvent &event)
 {
-
 
         if( mKeyboard->isKeyDown(OIS::KC_ESCAPE) || mKeyboard->isKeyDown(OIS::KC_Q) )
 			return false;
@@ -58,31 +46,9 @@ bool LoginListener::processUnbufferedKeyInput(const FrameEvent &event)
 			mTimeUntilNextToggle = 0.5;
 		}
 
-
-    	if(mKeyboard->isKeyDown(OIS::KC_UP) )
-            dummy->getBody()->addForce(Vector3(0,0,1) );
-    	if(mKeyboard->isKeyDown(OIS::KC_DOWN) )
-            dummy->getBody()->addForce(Vector3(0,0,-1) );
-    	if(mKeyboard->isKeyDown(OIS::KC_RIGHT) )
-            dummy->getBody()->addForce(Vector3(-1,0,0) );
-    	if(mKeyboard->isKeyDown(OIS::KC_LEFT) )
-            dummy->getBody()->addForce(Vector3(1,0,0) );
-
-    	if(mKeyboard->isKeyDown(OIS::KC_W) )
-            dummy->getBody()->addForce(Vector3(00,5,0) );
-
-    	if(mKeyboard->isKeyDown(OIS::KC_S) )
-            dummy->getBody()->addForce(Vector3(00,-5,0) );
-
-
-    	if(mKeyboard->isKeyDown(OIS::KC_P) )
-                fprintf(stderr,"%f, %f, %f",dummy->getNode()->getPosition().x,dummy->getNode()->getPosition().y,dummy->getNode()->getPosition().z);
-
         if (mKeyboard->isKeyDown(OIS::KC_E))
              m_world->setShowDebugGeometries(!m_world->getShowDebugGeometries());
 
-		if( mKeyboard->isKeyDown(OIS::KC_F2))
-            Ogre::Root::getSingleton().showConfigDialog();
     return true;
 }
 
